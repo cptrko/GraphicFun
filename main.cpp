931e2844952ac26ec43c5edd305c220f06fedb8a
Szymonjac <szymek.j@vp.pl>
2018-08-08 23:32:20 +0200

    testowanie

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
