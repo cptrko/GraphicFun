f9fd63b72c1efa278db5eee8f5a77b38416ce8a8
Szymonjac <szymek.j@vp.pl>
2018-08-09 05:05:23 +0200

    Add wallpaper in file and creating files for the function

---
+++ Menu-EOG.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+
+
+void EndOfGame()
+{
+
+
+
+
+
+}
---
+++ menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+void menu();
+void EndOfGame();
+
+
+#endif // MENU_H_INCLUDED
